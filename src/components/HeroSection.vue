a35dc22f4270232da8110631020317895416d745
Khoerul Fajri <khoerulfajri1999@gmail.com>
2025-06-15 14:18:08 +0700

    -fixed: fix trigger animation in HeroSection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -22,125 +22,125 @@
   </template>
   
   <script>
-  import gsap from "gsap";
-  
-  export default {
-      name: "HeroSection",
-      data() {
-        return {
-          images: [
-            require('../assets/images/hero/hero1.svg'),
-            require('../assets/images/hero/hero2.svg')
-          ],
-          currentIndex: 0,
-          intervalId: null,
-          hasAnimated: false,
-        };
-      },
-      computed: {
-        currentImage() {
-          return this.images[this.currentIndex];
-        },
-      },
-      mounted() {
-        // IntersectionObserver untuk mendeteksi saat section tampil
-        const options = {
-          root: null,
-          rootMargin: "0px",
-          threshold: 0.5,
-        };
-        this.observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting && !this.hasAnimated) {
-              this.animateHero();
-              this.hasAnimated = true;
-            }
-          });
-        }, options);
-  
-        this.observer.observe(this.$refs.hero);
-      },
-      beforeUnmount() {
-        clearInterval(this.intervalId);
-        if(this.observer) {
-          this.observer.disconnect();
-        }
-      },
-      methods: {
-        animateHero() {
-          gsap.from(this.$refs.heroImage, {
-            y: -100,
-           opacity: 0,
-            duration: 2,
-            ease: "power4.out",
-          });
-  
-          gsap.from(this.$refs.heroTitle, {
-            x: 100,
-           opacity: 0,
-            duration: 2,
-            ease: "power4.out",
-          });
-  
-          gsap.from(this.$refs.heroParagraph, {
-            y: 100,
-           opacity: 0,
-            duration: 2,
-            ease: "power4.out",
-          });
-  
-          gsap.from(this.$refs.socialIcons, {
-            y: 100,
-           opacity: 0,
-            duration: 2,
-            ease: "power4.out",
-          });
-  
-          gsap.from(this.$refs.heroBtn, {
-            y: 100,
-           opacity: 0,
-            duration: 2,
-            ease: "power4.out",
-          });
-  
-          // Animasi arrow naik turun
-          gsap.to(this.$refs.arrow, {
-            y: -10,
-            scale: 0.1,
-            duration: 2,
-            repeat: -1,
-            yoyo: true,
-            ease: "power1.inOut",
-          });
-  
-          this.startSlideshow();
-        },
-        startSlideshow() {
-          this.intervalId = setInterval(this.nextImage, 3000);
-        },
-        nextImage() {
-          gsap.to(this.$refs.heroImage, {
-              duration: 0.2,
-              y: 400,
-            opacity: 0,
-              ease: "power.inOut",
-              onComplete: () => {
-                this.currentIndex = (this.currentIndex + 1) % this.images.length;
-  
-                gsap.set(this.$refs.heroImage, { y: -400,opacity: 0 });
-  
-                gsap.to(this.$refs.heroImage, {
-                  duration: 0.2,
-                  y: 0,
-                opacity: 1,
-                  ease: "power.inOut",
-                });
-              },
-          });
-        },
-      },
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default {
+  name: "HeroSection",
+  data() {
+    return {
+      images: [
+        require("../assets/images/hero/hero1.svg"),
+        require("../assets/images/hero/hero2.svg"),
+      ],
+      currentIndex: 0,
+      intervalId: null,
     };
-  </script>
+  },
+  computed: {
+    currentImage() {
+      return this.images[this.currentIndex];
+    },
+  },
+  mounted() {
+    this.animateHero();
+    this.startSlideshow();
+  },
+  beforeUnmount() {
+    clearInterval(this.intervalId);
+  },
+  methods: {
+    animateHero() {
+      // Animasi gambar saat section mulai tampak di layar
+      gsap.from(this.$refs.heroImage, {
+        scrollTrigger: {
+          trigger: this.$refs.hero,
+          start: "top 80%", // saat section 20% di atas bottom layar
+          toggleActions: "play reset play reset", // Memutar ulang animasi saat masuk dan reset saat keluar
+        },
+        y: -100,
+        opacity: 0,
+        duration: 2,
+        ease: "power4.out",
+      });
+
+      // Animasi judul
+      gsap.from(this.$refs.heroTitle, {
+        scrollTrigger: {
+          trigger: this.$refs.hero,
+          start: "top 80%",
+          toggleActions: "play reset play reset",
+        },
+        x: 100,
+        opacity: 0,
+        duration: 2,
+        ease: "power4.out",
+      });
+
+      // Animasi paragraf
+      gsap.from(this.$refs.heroParagraph, {
+        scrollTrigger: {
+          trigger: this.$refs.hero,
+          start: "top 80%",
+          toggleActions: "play reset play reset",
+        },
+        y: 100,
+        opacity: 0,
+        duration: 2,
+        ease: "power4.out",
+      });
+
+      // Animasi ikon sosial
+      gsap.from(this.$refs.socialIcons, {
+        scrollTrigger: {
+          trigger: this.$refs.hero,
+          start: "top 80%",
+          toggleActions: "play reset play reset",
+        },
+        y: 100,
+        opacity: 0,
+        duration: 2,
+        ease: "power4.out",
+      });
+
+      // Animasi arrow naik turun
+      gsap.to(this.$refs.arrow, {
+        y: -10,
+        scale: 0.1,
+        duration: 2,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+      });
+    },
+    startSlideshow() {
+      this.intervalId = setInterval(this.nextImage, 3000);
+    },
+    nextImage() {
+      gsap.to(this.$refs.heroImage, {
+        duration: 0.2,
+        y: 400,
+        opacity: 0,
+        ease: "power.inOut",
+        onComplete: () => {
+          this.currentIndex = (this.currentIndex + 1) % this.images.length;
+
+          gsap.set(this.$refs.heroImage, { y: -400, opacity: 0 });
+
+          gsap.to(this.$refs.heroImage, {
+            duration: 0.2,
+            y: 0,
+            opacity: 1,
+            ease: "power.inOut",
+          });
+        },
+      });
+    },
+  },
+};
+</script>
   
   <style lang="sass" scoped>
   @import "@/assets/styles/HeroStyle.sass";
